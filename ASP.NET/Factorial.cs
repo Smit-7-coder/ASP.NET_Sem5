35f558ba1818cd72030c104f0904a4fabfc35cc1
Sakariya Smit R <smit7.sakariya@gmail.com>
2026-07-12 16:04:09 +0530

    Updates in Code

--- ASP.NET/Factorial.cs
+++ ASP.NET/Factorial.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; // Imports the System namespace for Console and Convert classes
+using System.Collections.Generic; // Imports classes for generic collections
+using System.Linq; // Imports LINQ functionality
+using System.Text; // Imports classes for text handling
+using System.Threading.Tasks; // Imports classes for asynchronous programming
 
-namespace ASP.NET
+namespace ASP.NET // Defines a namespace named ASP.NET
 {
-    internal class Factorial
+    internal class Factorial // Defines an internal class named Factorial
     {
-        public static void fact()
+        public static void fact() // Defines a public static method named fact
         {
-            int number;
-            Console.Write("Enter Number For Factorial: ");
-            String str = Console.ReadLine();
+            int number; // Declares an integer variable named number
 
-            number = Convert.ToInt32(str);
-            int fact = 1;
-            for (int i = 1; i <= number; i++)
+            Console.Write("Enter Number For Factorial: "); // Displays a message to enter a number
+
+            String str = Console.ReadLine(); // Reads input from the user and stores it as a string
+
+            number = Convert.ToInt32(str); // Converts the string input into an integer
+
+            int fact = 1; // Declares and initializes factorial variable with 1
+
+            for (int i = 1; i <= number; i++) // Loops from 1 up to the entered number
             {
-                fact *= i;
+                fact *= i; // Multiplies fact by the current value of i
             }
-            Console.WriteLine($"the factorial of {number} is {fact}");
+
+            Console.WriteLine($"the factorial of {number} is {fact}"); // Displays the factorial result
         }
     }
 }
---
+++ ASP.NET/SumOfNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ASP.NET
+{
+    internal class SumOfNumbers
+    {
+        public static void sum()
+        {
+            int a, b, c, result;
+
+            Console.Write("Enter Number 1: ");
+            string str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            Console.Write("Enter Number 2: ");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            Console.Write("Enter Number 3: ");
+            str = Console.ReadLine();
+            c = Convert.ToInt32(str);
+
+            result = Sum(a, b, c);
+
+            Console.WriteLine($"Sum of {a}, {b} and {c} is {result}");
+
+            Console.Read();
+        }
+
+        static int Sum(int x, int y, int z)
+        {
+            int res;
+            res = x + y + z;
+            return res;
+        }
+    }
+}
