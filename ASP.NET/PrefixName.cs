dc8a314d52fb041a12f2f8c6856a327805e92dfc
Sakariya Smit R <smit7.sakariya@gmail.com>
2026-07-12 16:30:32 +0530

    Added PrefixName and PrintName

---
+++ ASP.NET/PrefixName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ASP.NET
+{
+    internal class PrefixName
+    {
+        public static void prefix()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Gender (M/F): ");
+            char gender = Convert.ToChar(Console.ReadLine());
+
+            if (gender == 'M' || gender == 'm')
+            {
+                Console.WriteLine("Mr. " + name);
+            }
+            else if (gender == 'F' || gender == 'f')
+            {
+                Console.WriteLine("Ms. " + name);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Gender");
+            }
+        }
+    }
+}
---
+++ ASP.NET/PrintName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ASP.NET
+{
+    internal class PrintName
+    {
+        public static void print()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Name: " + name);
+        }
+    }
+}
