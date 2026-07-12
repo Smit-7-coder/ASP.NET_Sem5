93170edbd9420d81872523125f39b13ce95a6b81
Sakariya Smit R <smit7.sakariya@gmail.com>
2026-07-12 16:19:05 +0530

    Added Somew New Files

---
+++ ASP.NET/Table_WhileLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ASP.NET
+{
+    internal class Table_WhileLoop
+    {
+        public static void table()
+        {
+            int num1, res, i;
+
+            Console.Write("Enter a Number: ");
+            string str = Console.ReadLine();
+
+            num1 = Convert.ToInt32(str);
+
+            i = 1;
+
+            while (i <= 10)
+            {
+                res = num1 * i;
+                Console.WriteLine($"{num1} x {i} = {res}");
+
+                i++;
+            }
+        }
+    }
+}
---
+++ ASP.NET/ToggleCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ASP.NET
+{
+    internal class ToggleCase
+    {
+        public static void toggle()
+        {
+            Console.Write("Enter a Name: ");
+            string str = Console.ReadLine();
+
+            string result = "";
+
+            foreach (char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    result += char.ToLower(ch);
+                }
+                else if (char.IsLower(ch))
+                {
+                    result += char.ToUpper(ch);
+                }
+                else
+                {
+                    result += ch;
+                }
+            }
+
+            Console.WriteLine("Toggle Case: " + result);
+        }
+    }
+}
